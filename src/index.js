91c6c4fedb2d8f5b87f0a0d77e461ed59924c646
serhiishf <Vesi@etra.ee>
2022-01-14 15:47:43 +0200

    feat: full functionality implemented

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
 module.exports = function toReadable(number) {
-    let arrStart = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eigth', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let arrFinish = ['emty', 'emty', 'twelve', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred']
+    let arrStart = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arrFinish = ['emty', 'emty', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred']
     let result = '';
     let numString = String(number);
     let first = Number(numString[0]);
     let second = Number(numString[1]);
     let third = Number(numString[2]);
-/*   
-    if (number > 99 && number < 1000) {
+    if (numString.length >= 3 && second + third !== 0) {
         result = arrStart[first] + ` hundred `;
-        number = Number(second + third);
-        if (number >= 0 && number < 20) {
-            result = result + arrStart[number];
-        }
-        else if (number > 19 && number < 100 && second !== 0) {
-            result = result + arrFinish[first] + ` ` + arrStart[second];
-        }
-        else if (number > 19 && number < 100 && second === 0) {
-            result = result + arrFinish[first]
-        }
+        number = String(second) + String(third);
+        number = Number(number);
+        first = second;
+        second = third;
     }
-*/
+
     if (number >= 0 && number < 20) {
-        result = arrStart[number];
+        result = result + arrStart[number];
     }
     else if (number > 19 && number < 100 && second !== 0) {
-        result = arrFinish[first] + ` ` + arrStart[second];
+        result = result + arrFinish[first] + ` ` + arrStart[second];
     }
     else if (number > 19 && number < 100 && second === 0) {
-        result = arrFinish[first]
+        result = result + arrFinish[first]
+    }
+    else if (numString.length >= 3 && second === 0 && third === 0) {
+        result = arrStart[first] + ` hundred`;
     }
 
 
